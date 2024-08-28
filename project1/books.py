ac710b58c70244a6d084dd406ea82e6d08f66c7c
Wiktor Piela <wpiela9@gmail.com>
2024-08-28 12:04:50 +0200

    main.py renamed

---
+++ project1/books.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+]
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/abc")
+async def abc():
+    return {"message": "Hello World2222"}
